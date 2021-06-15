804f64812caf51fdd6068f10284d5535e3e84edc
Roman Kolmakov <cerne313@gmail.com>
2021-06-15 21:29:36 +0300

    fix bootstrap.js, implement db creation

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
+import AppLoading from 'expo-app-loading';
 
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './src/navigation/AppNavigation';
 import store from './src/store';
+import { bootstrap } from './src/bootstrap';
 
 export default function App() {
-	// const [isReady, setIsReady] = useState(false);
+	const [isReady, setIsReady] = useState(false);
 
-	// if (!isReady) {
-	// 	return (
-	// 		<AppLoading
-	// 			startAsync={bootstrap}
-	// 			onFinish={() => setIsReady(true)}
-	// 			onError={(err) => console.log(err)}
-	// 		/>
-	// 	);
-	// }
+	if (!isReady) {
+		return (
+			<AppLoading
+				startAsync={bootstrap}
+				onFinish={() => setIsReady(true)}
+				onError={(err) => console.log(err)}
+			/>
+		);
+	}
 
 	return (
 		<Provider store={store}>
